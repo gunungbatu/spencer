fe7268e5f3da239202272b062098927f55c8e7be
gunungbatu <gunungbatuvilla@gmail.com>
2025-12-18 11:35:44 +0700

    Delete get_config.php

--- get_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$config = json_decode(file_get_contents('config.json'), true);
-echo json_encode([
-    "api_url" => $config['api_url'] ?? '',
-    "hotel_wa" => $config['hotel_wa'] ?? ''
-]);
-?>
